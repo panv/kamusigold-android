c1705ff78da8a7c425dee0b3b6a69c5f34a2cb00
panv <panv@hotmail.fr>
2017-03-25 21:27:57 +0100

    Add angular-gettext to project

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+
+    // Project configuration
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+        nggettext_extract: {
+            pot: {
+                files: {
+                    'www/po/template.pot': ['www/index.html']
+                }
+            }
+        },
+        nggettext_compile: {
+            all: {
+                files: {
+                    'www/translations/translations.js': ['www/po/*.po']
+                }
+            }
+        }
+    });
+
+    // Load angular-gettext plugin
+    grunt.loadNpmTasks('grunt-angular-gettext');
+
+    // Default task
+    grunt.registerTask('default', ['nggettext_extract', 'nggettext_compile']);
+    // extract and compile tasks
+    grunt.registerTask('extract', ['nggettext_extract']);
+    grunt.registerTask('compile', ['nggettext_compile']);
+
+};
